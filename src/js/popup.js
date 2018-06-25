fda5ec4539db295e1eef7f9b3e9d658c8e25f2e7
botvinick <benbotvinick@gmail.com>
2018-06-25 13:47:43 -0400

    Finish first version

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
 	$('#safari').on('click', function () {
 		chrome.tabs.executeScript({
-			file: 'js/safari.js'
+			file: 'js/browsers/safari.js'
+		});
+	});
+	$('#opera').on('click', function () {
+		chrome.tabs.executeScript({
+			file: 'js/browsers/opera.js'
+		});
+	});
+	$('#firefox').on('click', function () {
+		chrome.tabs.executeScript({
+			file: 'js/browsers/firefox.js'
+		});
+	});
+	$('#internet-explorer').on('click', function () {
+		chrome.tabs.executeScript({
+			file: 'js/browsers/internet-explorer.js'
 		});
 	});
 });
